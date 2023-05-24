b828a772b4e93e0f66e1b9003fd8176186e66557
Paulo <pauloh16.pdr@gmail.com>
2023-05-24 18:34:50 -0300

    feat: organizar estrutura de arquivos

--- App.tsx
+++ App.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
-
+import Home from './src/screen/Home';
 
 const App: React.FC = () => {
   return (
-   <View
-      style={{
-        padding: 24,
-        backgroundColor: '#131016',
-        flex: 1,
-      }}
-   >
-     <Text
-      style={{
-        marginTop: 48,
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#FDFEFD',
-      }}
-     >
-      Nome do evento
-    </Text>
-
-     <Text
-      style={{
-        fontSize: 16,
-        color: '#6B6B6B',
-      }}
-    >
-      Sexta, 4 de novembro de 2022</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#131016' }}>
+      <Home />
+    </SafeAreaView>
   );
 };
 
---
+++ src/screen/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Styles from './styles';
+
+const Home: React.FC = () => {
+  return (
+   <View style={Styles.container}>
+    <Text style={Styles.eventName}>Nome do evento</Text>
+
+    <Text style={Styles.eventDate}>
+      Sexta, 4 de novembro de 2022</Text>
+    </View>
+  );
+};
+
+export default Home;

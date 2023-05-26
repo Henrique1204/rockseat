28dc7354fe8c44455256c02231fb2590b624d151
Paulo <pauloh16.pdr@gmail.com>
2023-05-25 22:45:58 -0300

    feat: adicionar exemplo de statusBar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 
 import Home from './src/screen/Home';
 
 const App: React.FC = () => {
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: '#131016' }}>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
 			<Home />
 		</SafeAreaView>
 	);
